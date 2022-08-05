0decfdfa7900178cf9f173ef7cddd1f572cbdb3c
ana-gujabidze <ana.gujabidze@quantori.com>
2022-08-05 18:52:03 +0400

    Update test cases

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,8 +7,6 @@ import unittest
 from requests.exceptions import JSONDecodeError
 from src import app
 
-base_url = "https://www.ebi.ac.uk/pdbe/graph-api/compound/summary/"
-
 
 class TestRequests(unittest.TestCase):
 
@@ -20,8 +18,8 @@ class TestRequests(unittest.TestCase):
         self.assertRaises(JSONDecodeError, app.pull_data, '')
 
     def test_wrong_input(self):
-        """T
-        est when wrong compound name is concatenated to the base URL,
+        """
+        Test when wrong compound name is concatenated to the base URL,
         empty JSON is returned.
         """
         result = app.pull_data('aTP')
@@ -33,21 +31,28 @@ class TestRequests(unittest.TestCase):
         JSON in populated appropriately.
         """
         result = app.pull_data("ATP")
-        name = result["ATP"][0]['name']
-        compound = list(result.keys())[0]
-        formula = result["ATP"][0]['formula']
-        inchi = result["ATP"][0]['inchi']
-        inchi_key = result["ATP"][0]['inchi_key']
-        smiles = result["ATP"][0]['smiles']
-        cross_links_count = len(result["ATP"][0]['cross_links'])
 
-        self.assertEqual("ADENOSINE-5'-TRIPHOSPHATE", name)
-        self.assertEqual("ATP", compound)
-        self.assertEqual("C10 H16 N5 O13 P3", formula)
-        self.assertEqual("InChI=1S/C10H16N5O13P3/c11-8-5-9(13-2-12-8)15(3-14-5)10-7(17)6(16)4(26-10)1-25-30(21,22)28-31(23,24)27-29(18,19)20/h2-4,6-7,10,16-17H,1H2,(H,21,22)(H,23,24)(H2,11,12,13)(H2,18,19,20)/t4-,6-,7-,10-/m1/s1", inchi)
-        self.assertEqual("ZKHQWZAMYRWXGA-KQYNXXCUSA-N", inchi_key)
-        self.assertEqual("c1nc(c2c(n1)n(cn2)C3C(C(C(O3)COP(=O)(O)OP(=O)(O)OP(=O)(O)O)O)O)N", smiles)
-        self.assertEqual(23, cross_links_count)
+        test_data = {
+            "compound": "ATP",
+            "name":"ADENOSINE-5'-TRIPHOSPHATE",
+            "formula": "C10 H16 N5 O13 P3",
+            "inchi": "InChI=1S/C10H16N5O13P3/c11-8-5-9(13-2-12-8)15(3-14-5)10-7(17)6(16)4(26-10)1-25-30(21,22)28-31(23,24)27-29(18,19)20/h2-4,6-7,10,16-17H,1H2,(H,21,22)(H,23,24)(H2,11,12,13)(H2,18,19,20)/t4-,6-,7-,10-/m1/s1",
+            "inchi_key": "ZKHQWZAMYRWXGA-KQYNXXCUSA-N",
+            "smiles": "c1nc(c2c(n1)n(cn2)C3C(C(C(O3)COP(=O)(O)OP(=O)(O)OP(=O)(O)O)O)O)N",
+            "cross_links_count": 23
+        }
+
+        retrieved_data = {
+            "compound": list(result.keys())[0],
+            "name": result["ATP"][0]['name'],
+            "formula": result["ATP"][0]['formula'],
+            "inchi": result["ATP"][0]['inchi'],
+            "inchi_key": result["ATP"][0]['inchi_key'],
+            "smiles": result["ATP"][0]['smiles'],
+            "cross_links_count": len(result["ATP"][0]['cross_links'])
+        }
+
+        self.assertDictEqual(test_data, retrieved_data)
 
 
 if __name__ == '__main__':
